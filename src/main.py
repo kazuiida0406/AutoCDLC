ae7ffd104f43c336ce864f6f8b993908fe0f2630
kazuiida0406 <kazuiida0406@gmail.com>
2026-08-01 20:29:02 +0900

    Create project structure

---
+++ src/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("===================================")
+    print(" AutoCDLC")
+    print(" GP7 -> Rocksmith CDLC")
+    print("===================================")
+
+
+if __name__ == "__main__":
+    main()
